ba7714597515369bfee88fc1d416fb726e0ef95c
Alek Sai <hi@aleksai.pro>
2024-12-19 19:12:51 +0300

    Clean up

--- Shared/Common/Colors.swift
+++ Shared/Common/Colors.swift
@@ -1,9 +1,7 @@
 import UIKit
 
 extension XXColor {
-    static let ball = UIColor(red: 212 / 255, green: 122 / 255, blue: 1, alpha: 1)
-
+    static let ball = UIColor(red: 102 / 255, green: 24 / 255, blue: 1, alpha: 1)
     static let floor = UIColor.white
-
     static let barrel = UIColor(red: 123 / 255, green: 122 / 255, blue: 1, alpha: 1)
 }
